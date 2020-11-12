ef49a84b533528c0b1cd57e0d8e8a4d8cf56d6ed
jonhowell1 <jon@jon-howell.co.uk>
2020-11-12 14:57:53 +0000

    Add files via upload

---
+++ TechTestExample/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechTestExample.Models
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public int CompanyNumber { get; set; }
+        public string Postcode { get; set; }
+        public bool Active { get; set; }
+        public string Sector { get; set; }
+
+        public List<ContactDetails> ContactDetails = new List<ContactDetails>();
+    }
+
+    public class ChildCompany : Company
+    {
+        public string ParentCompany { get; set; }
+    }
+}
---
+++ TechTestExample/Models/ContactDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechTestExample.Models
+{
+    public class ContactDetails
+    {
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
